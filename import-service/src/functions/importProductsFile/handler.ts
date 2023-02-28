250bd55651e39d6c36f6e5c450b78cbbb18b40ae
Yulia Ivanova <yulia_ivanova@epam.com>
2023-02-28 23:58:08 +0300

    feat: Task 5

---
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -0,0 +1,29 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+import { formatJSONResponse } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+import schema from './schema';
+
+const BUCKET = 'yi-import-service-bucket';
+const REGION = "eu-west-1";
+const s3Client = new S3Client({ region: REGION });
+
+const importProductsFile: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  const { name } = event.queryStringParameters;
+  const params = {
+    Bucket: BUCKET,
+    Key: `uploaded/${name}`,
+    ContentType: 'text/csv'
+  }
+  const command = new PutObjectCommand(params);
+  const signedUrl = await getSignedUrl(s3Client, command, {
+    expiresIn: 3600,
+  });
+  return formatJSONResponse({
+    signedUrl
+  });
+};
+
+export const main = middyfy(importProductsFile );
---
+++ import-service/src/libs/api-gateway.ts
@@ -0,0 +1,23 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from "aws-lambda"
+import type { FromSchema } from "json-schema-to-ts";
+
+type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
+export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
+
+export const formatJSONResponse = (response: Record<string, unknown>) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response)
+  }
+}
+
+export const errorResponse = (response: Error) => {
+  return {
+    statusCode: 500,
+    body: JSON.stringify(response.message),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+  }
+}
